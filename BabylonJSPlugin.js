e65f54b633a93d400ecaf7c15d7c50af942931df
alecell <alecell.dev@gmail.com>
2023-01-25 00:23:15 -0300

    refactor: make it more readable

--- BabylonJSPlugin.js
+++ BabylonJSPlugin.js
@@ -3,61 +3,79 @@ const getLogger = require('webpack-log');
 const path = require('path');
 const log = getLogger({ name: 'webpack-batman' });
 
-const getTextures = (materials) => {
-  const paths = materials.map(material => {
-    return `${material.diffuseTexture.name}`;
-  })
+const PLUGIN_NAME = 'BabylonJSPlugin';
+const GET_SCENE_LOADER_METHODS_PATH_REGEX = /SceneLoader\.ImportMeshAsync\(\s*['"].*['"],\s*['"](?<path>.*)['"],\s*['"](?<fileName>.*)['"],\s*.*\)/gm
 
-  return [...new Set(paths)]
-}
+/**
+ * TODO: Work properly with development and production environments
+ * TODO: Make it work with other methods other than ImportMeshAsync
+ * https://doc.babylonjs.com/typedoc/classes/BABYLON.SceneLoader
+ * 
+ * Provavelmente oq eu vou fazer vai ser, puxar os arquivos a serem alterados
+ * antes do build, fazer a busca pelo metodo SceneLoader e criar o fakePath que aponta pra ./modules/<hash>/
+ * e guardar no array de files
+ * 
+ * Depois no after build pegar esse array e fazer as cópias dos roles
+ */
 
-const copyFileWithFolders = async (src, dest) => {
-  const folders = dest.split(path.sep).slice(0, -1);
-  let currentPath = '';
-
-  console.log(src, dest)
-  if (!fs.existsSync(path.dirname(dest))) {
-    for (const folder of folders) {
-      currentPath += folder + path.sep;
-
-      try {;
-        await fs.promises.access(currentPath);
-      } catch(e) {
-        await fs.promises.mkdir(currentPath);
-      }
-    }
-  }
-
-  return fs.promises.copyFile(src, dest);
-}
-
-const copyFilesToDist = async (files, sourcePath, distPath) => {
-  const copyPromises = files.map(file => copyFileWithFolders(`${sourcePath}${file}`, `./dist/${file}`))
-  return Promise.all(copyPromises);
-}
 
 class BabylonJSPlugin {
-  async findAndCopyAssets(fullPath, content) {
-    const regExp = /SceneLoader\.ImportMeshAsync\(\s*['"].*['"],\s*['"](?<path>.*)['"],\s*['"](?<fileName>.*)['"],\s*.*\)/gm;
-    const basePath = `${path.dirname(fullPath)}/`;
+  files = [];
+
+  getTexturesFromBabylonMesh(materials) {
+    const paths = materials.map(material => {
+      return `${material.diffuseTexture.name}`;
+    })
+  
+    return [...new Set(paths)]
+  }
+
+  async createFoldersAndCopyFile(src, dest) {
+    await fs.promises.mkdir(path.dirname(dest), { recursive: true });
+   
+    const buffer = Buffer.alloc(1024 * 1024);
+    const readStream = fs.createReadStream(src);
+    const writeStream = fs.createWriteStream(dest);
+   
+    readStream.pipe(writeStream);
+   
+    return new Promise((resolve, reject) => {
+      readStream.on('end', resolve);
+      writeStream.on('error', reject);
+    });
+  }
+
+  async copyFilesToDist(files, filesPath, distPath) {
+    const copyPromises = files.map(file => this.createFoldersAndCopyFile(`${filesPath}${file}`, `${distPath}${file}`))
+    return Promise.all(copyPromises);
+  }
+
+  async findMeshAndTextures(scriptPath, match) {
+    const meshPath = `${scriptPath}${match.path}${match.fileName}`;
+    const mesh = JSON.parse(await fs.promises.readFile(meshPath, 'utf8'));
+    const materials = mesh.materials;
+    const textures = this.getTexturesFromBabylonMesh(materials, scriptPath, match.path);
+
+    return [
+      match.fileName,
+      ...textures
+    ]
+  }
+  
+  async findAndCopyAssets(scriptFilePath, content) {
+    const regExp = GET_SCENE_LOADER_METHODS_PATH_REGEX;
+    const scriptPath = `${path.dirname(scriptFilePath)}/`;
     let match = regExp.exec(content)?.groups;
     
     while (match) {
-      const modelPath = `${basePath}${match.path}${match.fileName}`;
-      const modelContent = JSON.parse(await fs.promises.readFile(modelPath, 'utf8'));
-      const materials = modelContent.materials;
-      const textures = getTextures(materials, basePath, match.path);
-      const distPath = path.normalize(`./dist/${match.path}`);
-      const files = [
-        match.fileName,
-        ...textures
-      ]
-
-      await copyFilesToDist(files, path.normalize(`${basePath}${match.path}`), distPath);
+      const files = await this.findMeshAndTextures(scriptPath, match);
+      const distPath = path.normalize(`${process.cwd()}/dist/${match.path}`);
+      await this.copyFilesToDist(files, path.normalize(`${scriptPath}${match.path}`), distPath);
       match = regExp.exec(content)?.groups;
     }
   }
 
+  //  PROVAVELMENTE N VOU MAIS USAR, ENTÃO SEM REFACTOR
   async getAllTsFiles(dir) {
     const dirents = await fs.promises.readdir(dir, { withFileTypes: true });
     const files = await Promise.all(dirents.map(async (dirent) => {
@@ -71,21 +89,25 @@ class BabylonJSPlugin {
     return files.flat().filter(item => item !== undefined);
   }
 
-  apply(compiler) {
-    compiler.hooks.afterEmit.tapAsync('BabylonJSPlugin', (compilation, callback) => {
-      this.getAllTsFiles('./src')
-        .then((paths) => {
-          paths.forEach(path => {
-            fs.promises.readFile(path, 'utf8')
-              .then(content => this.findAndCopyAssets(path, content))
-          })
-        })
-        .catch(err => {
-          console.log(err)
-        });
+  copy3DModelsAndTexturesToDist(compilation, callback) {
+    // console.log(compilation.modules)
 
-      callback();
-    });
+    this.getAllTsFiles('./src')
+      .then((paths) => {
+        paths.forEach(path => {
+          fs.promises.readFile(path, 'utf8')
+            .then(content => this.findAndCopyAssets(path, content))
+        })
+      })
+      .catch(err => {
+        console.log(err)
+      });
+
+    callback();
+  }
+
+  apply(compiler) {
+    compiler.hooks.afterEmit.tapAsync(PLUGIN_NAME, this.copy3DModelsAndTexturesToDist.bind(this));
   }
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,7 @@ module.exports = {
   devServer: {
     host: "0.0.0.0",
     port: 8080, //port that we're using for local host (localhost:8080)
-    static: path.resolve(appDirectory, "public"), //tells webpack to serve from the public folder
+    static: path.resolve(appDirectory, "dist"), //tells webpack to serve from the public folder
     hot: true,
     devMiddleware: {
       publicPath: "/",
